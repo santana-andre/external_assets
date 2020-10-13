2efd381da40d6542f190bbbf4a03659b6ff6042f
Felipe Fregulhia <felipe.fregulhia@nsh.inf.br>
2020-10-13 14:31:29 -0300

    Update occ.js

--- occ.js
+++ occ.js
@@ -1,27 +1,4 @@
 //$("body").append("<script src='https://150.136.166.80:3000/occjs' cliente='teste'></script>")
-//Lista de Clientes
-// var json = {
-//     oi: {
-//         logo: "https://s2.glbimg.com/KkAhF9jrHGK9qan2sf8LuiQ-nME=/1200x/smart/filters:cover():strip_icc()/i.s3.glbimg.com/v1/AUTH_63b422c2caee4269b8b34177e8876b93/internal_photos/bs/2020/v/i/glx2xMRgW5kBTOK7OBdg/logooi140x140.png",
-//         cor_link: "green",
-//         cor_btn_txt: "#fff",
-//         cor_btn_back: "#a94442",
-//         cor_btn_borda: "transparent",
-//         banner: "https://png.pngtree.com/thumb_back/fw800/back_pic/03/88/04/9357d3ea7e6f9cc.jpg",
-//         font: "Montserrat",
-//         linkfont: "https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-//     },
-//     rv: {
-//         logo: "https://www.rivershop.com.br/file/general/logo_login.png",
-//         cor_link: "green",
-//         cor_btn_txt: "#fff",
-//         cor_btn_back: "#a94442",
-//         cor_btn_borda: "transparent",
-//         banner: "https://png.pngtree.com/thumb_back/fw800/back_pic/03/88/04/9357d3ea7e6f9cc.jpg",
-//         font: "Long Cang",
-//         linkfont: "https://fonts.googleapis.com/css2?family=Long+Cang&display=swap"
-//     }
-// }
 
 function getSyncScriptParams() {
     var scripts = document.getElementsByTagName('script');
