045cb18d5ad4839be232ed08586b47e3690e8cb9
André Santana <andre@desantana.com.br>
2020-11-27 20:40:13 -0300

    Update occ.js
    Alterado url de "https://193.122.145.73:3000/v1/external_assets"; para "https://193.122.145.73:3000/external_assets";

--- occ.js
+++ occ.js
@@ -1,4 +1,4 @@
-//$("body").append("<script src='https://150.136.166.80:3000/occjs' cliente='teste'></script>")
+//$("body").append("<script src='https://193.122.145.73:3000/occjs' cliente='teste'></script>")
 
 function getSyncScriptParams() {
     var scripts = document.getElementsByTagName('script');
@@ -11,7 +11,7 @@ function getSyncScriptParams() {
 //Get Cliente Param
 var cliente = getSyncScriptParams()
 
-var url_base = "https://193.122.145.73:3000/v1/external_assets";
+var url_base = "https://193.122.145.73:3000/external_assets";
 //---------------------------- Buscar customizações expecifica
 function getCustomer(callback) {
     var xmlHttp = new XMLHttpRequest();
